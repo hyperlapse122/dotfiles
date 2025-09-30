dc9174f9a1366a3cd61f0d2778479a5bccffb8cc
Joosung Park <iam@hyperlapse.dev>
2025-09-30 22:49:41 +0900

    chore: add bootstrap script and update installation flow
    Introduced a new `bootstrap.sh` script for setting up the environment, including Homebrew installation and cloning the dotfiles repository. Updated `install.sh` to streamline the installation process and removed redundant Homebrew logic. Added Google Drive to the Brewfile for better setup coverage.

---
+++ bootstrap.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env zsh
+
+set -xeuo pipefail
+
+# Install Homebrew
+if ! command -v brew &>/dev/null; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/hyperlapse/.zprofile
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
+
+brew install git git-lfs
+git-lfs install
+
+# Clone the repo then it's not cloned yet
+if [ ! -d "${ZDOTDIR:-$HOME}/.dotfiles" ]; then
+  git clone --recursive https://github.com/hyperlapse122/dotfiles.git "${ZDOTDIR:-$HOME}/.dotfiles"
+fi
+
+cd "${ZDOTDIR:-$HOME}/.dotfiles" && source install.sh
--- install.sh
+++ install.sh
@@ -4,19 +4,14 @@ set -xeuo pipefail
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# Install Homebrew
-if ! command -v brew &>/dev/null; then
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/hyperlapse/.zprofile
-  eval "$(/opt/homebrew/bin/brew shellenv)"
-fi
-
 # Install Homebrew formulae and casks
 brew bundle --file="$DIR/Brewfile"
 
-# Install prezto
-git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-git clone --recursive https://github.com/belak/prezto-contrib "${ZDOTDIR:-$HOME}/.zprezto/contrib"
+# Install prezto when it's not installed
+if [ ! -d "${ZDOTDIR:-$HOME}/.zprezto" ]; then
+  git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+  git clone --recursive https://github.com/belak/prezto-contrib "${ZDOTDIR:-$HOME}/.zprezto/contrib"
+fi
 
 MACOS_GHOSTTY_CONFIG="$HOME/Library/Application Support/com.mitchellh.ghostty/config"
 if [ -f "$MACOS_GHOSTTY_CONFIG" ]; then
@@ -27,4 +22,6 @@ stow -t "$HOME" zsh git mise ghostty gnupg zed
 
 # mise
 mise trust --all && mise install
+
+# Symlink mise to asdf for JetBrains IDEs
 ln -s ~/.local/share/mise ~/.asdf
