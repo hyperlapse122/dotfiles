23b6df4de06f4f91a1bd286834267edbc652e50f
Joosung Park <iam@h82.dev>
2025-10-13 15:11:17 +0900

    feat: update font installation script and configuration paths

--- fonts/InstallNerdFonts.ps1
+++ fonts/InstallNerdFonts.ps1
@@ -1,1 +1,1 @@
-& ([scriptblock]::Create((Invoke-WebRequest 'https://to.loredo.me/Install-NerdFont.ps1'))) -Name jetbrains-mono,d2coding
+& ([scriptblock]::Create((Invoke-WebRequest 'https://to.loredo.me/Install-NerdFont.ps1'))) -Name jetbrains-mono,d2coding -Confirm:$false
