65311ac7ac451ceebf18d7ab1fadaf31530c3797
Joosung Park <iam@h82.dev>
2025-10-17 13:33:31 +0900

    chore(vscode): add mcp configurations

--- Install.ps1
+++ Install.ps1
@@ -1,6 +1,9 @@
 $ErrorActionPreference = 'Stop'
 
-$CONFIG = "install-windows.conf.yaml"
+$CONFIG = $IsWindows ? "install-windows.conf.yaml" : "install.conf.yaml"
+$PYTHON_EXECUTABLE = $IsWindows ? "py" : "python"
+
+$ROOT_CONFIG = $IsWindows ? $null : "install-root.conf.yaml"
 $DOTBOT_DIR = "dotbot"
 $DOTBOT_BIN = "bin/dotbot"
 $BASEDIR = Split-Path -Parent $MyInvocation.MyCommand.Definition
@@ -15,4 +18,13 @@ Set-Location $BASEDIR
 git submodule sync --quiet --recursive
 git submodule update --init --recursive
 
-py "$BASEDIR/$DOTBOT_DIR/$DOTBOT_BIN" "--plugin" "dotbot-windows/dotbot_windows.py" "-d" "$BASEDIR" "-c" "$CONFIG" @args
+if($IsWindows) {
+    & "$PYTHON_EXECUTABLE" "$BASEDIR/$DOTBOT_DIR/$DOTBOT_BIN" "--plugin" "dotbot-windows/dotbot_windows.py" "-d" "$BASEDIR" "-c" "$CONFIG" @args
+}
+else {
+    & "$PYTHON_EXECUTABLE" "$BASEDIR/$DOTBOT_DIR/$DOTBOT_BIN" "-d" "$BASEDIR" "-c" "$CONFIG" @args
+}
+
+if ($ROOT_CONFIG -and -not $IsWindows) {
+    sudo "$PYTHON_EXECUTABLE" "$BASEDIR/$DOTBOT_DIR/$DOTBOT_BIN" "-d" "$BASEDIR" "-c" "$ROOT_CONFIG" @args
+}
