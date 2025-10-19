1cfb7b97315654a9d0e9d17d84670544e1db284f
Joosung Park <iam@h82.dev>
2025-10-19 15:57:16 +0900

    feat: update font submodules and configurations; add posix gitconfig and install scripts

--- fonts/InstallNerdFonts.ps1
+++
@@ -1,1 +0,0 @@
-& ([scriptblock]::Create((Invoke-WebRequest 'https://to.loredo.me/Install-NerdFont.ps1'))) -Name jetbrains-mono,d2coding -Confirm:$false
--- vscode/install.sh
+++ vscode/install.sh
@@ -3,4 +3,4 @@
 _CURR_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$_CURR_DIR" || exit 1
 
-cat "$_CURR_DIR/extensions.txt" | xargs -L 1 code --install-extension
+cat "$_CURR_DIR/extensions.txt" | xargs -L 1 -- code --force --install-extension
