c3b8b6797a5d43ed1e552aa2495f5738e1dc10f3
Joosung Park <iam@h82.dev>
2026-01-03 19:34:44 +0900

    chore(archinstall): add fonts

--- archinstall/initialize-user.sh
+++ archinstall/initialize-user.sh
@@ -12,7 +12,9 @@ cd ~ \
   && cd .. \
   && rm -rf yay
 
-yay -S 1password 1password-cli rust cargo qt5-base qt6-base git git-lfs mise kime-git visual-studio-code-bin gnupg zsh
+yay -S 1password 1password-cli rust cargo qt5-base qt6-base git git-lfs mise kime-git visual-studio-code-bin gnupg zsh \
+  otf-pretendard-jp otf-pretendard-std ttf-pretendard-gov ttf-pretendard-jp ttf-pretendard-std otf-pretendard ttf-pretendard otf-pretendard-gov \
+  ttf-jetbrains-mono ttf-jetbrains-mono-nerd
 
 cd ~ \
   && git clone https://github.com/hyperlapse122/dotfiles \
