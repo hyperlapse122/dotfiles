0c412899453ac83b8e7f3b65b104eb1c18ea65f8
Joosung Park <iam@h82.dev>
2026-01-05 21:03:44 +0900

    chore(archinstall): make user initialization non-interactive

--- archinstall/initialize-user.sh
+++ archinstall/initialize-user.sh
@@ -4,24 +4,46 @@ set -e -o pipefail
 
 _PWD=$(pwd)
 
-cd ~ \
-  && sudo pacman -S --needed git base-devel \
-  && git clone https://aur.archlinux.org/yay.git \
-  && cd yay \
-  && makepkg -si \
-  && cd .. \
-  && rm -rf yay
+# Ensure git + base-devel non-interactively
+cd ~
+sudo pacman -S --needed --noconfirm git base-devel
 
-yay -S 1password 1password-cli rust cargo qt5-base qt6-base git git-lfs mise kime-git visual-studio-code-bin gnupg zsh \
-  otf-pretendard-jp otf-pretendard-std ttf-pretendard-gov ttf-pretendard-jp ttf-pretendard-std otf-pretendard ttf-pretendard otf-pretendard-gov \
-  ttf-jetbrains-mono ttf-jetbrains-mono-nerd
+# Clone and build yay non-interactively
+if [ ! -d yay ]; then
+  git clone https://aur.archlinux.org/yay.git
+fi
 
-cd ~ \
-  && git clone https://github.com/hyperlapse122/dotfiles \
-  && cd dotfiles \
-  && mise trust --all \
-  && sh ./install.sh
+cd yay
+makepkg -si --noconfirm
+cd ..
+rm -rf yay
 
-chsh -s $(which zsh)
+# Configure yay once to always skip menus / questions
+yay --save \
+  --answerdiff None \
+  --answeredit None \
+  --answerclean None \
+  --nocleanmenu \
+  --nodiffmenu \
+  --noeditmenu
 
-cd $_PWD
+# Non-interactive yay install of all required packages
+yay -S --needed --noconfirm --mflags "--noconfirm" \
+  1password 1password-cli rust cargo qt5-base qt6-base mise kime-git visual-studio-code-bin \
+  otf-pretendard-jp otf-pretendard-std ttf-pretendard-gov ttf-pretendard-jp ttf-pretendard-std \
+  otf-pretendard ttf-pretendard otf-pretendard-gov
+
+# Clone dotfiles and run install non-interactively
+cd ~
+if [ ! -d dotfiles ]; then
+  git clone https://github.com/hyperlapse122/dotfiles
+fi
+
+cd dotfiles
+mise trust --all
+sh ./install.sh
+
+# Change shell non-interactively
+chsh -s "$(command -v zsh)"
+
+cd "$_PWD"
